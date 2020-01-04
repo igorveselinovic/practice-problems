1a9089b0e009ff314c2577679c0568b818f7de9f
Igor Veselinovic <igor@veselinovic.net>
2020-01-04 14:29:25 -0500

    Add "Is Unique" string problem

---
+++ crack/1_arrays_and_strings/1.1/IsUnique.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class IsUnique {
+    public static boolean isUnique(String s) {
+        HashSet<Character> set = new HashSet<Character>();
+        for (int i = 0, n = s.length(); i < n; i++) {
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                return false;
+            } else {
+                set.add(c);
+            }
+        }
+        return true;
+    }
+}
---
+++ crack/1_arrays_and_strings/1.1/IsUniqueTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class IsUniqueTest {
+    @Test
+    public void unique() {
+        assertEquals(true, IsUnique.isUnique("abc"));
+        assertEquals(true, IsUnique.isUnique("cba"));
+        assertEquals(true, IsUnique.isUnique("123abc"));
+        assertEquals(true, IsUnique.isUnique(""));
+        assertEquals(true, IsUnique.isUnique("a"));
+    }
+
+    @Test
+    public void notUnique() {
+        assertEquals(false, IsUnique.isUnique("aa"));
+        assertEquals(false, IsUnique.isUnique("aba"));
+        assertEquals(false, IsUnique.isUnique("1abcdef1"));
+    }
+}
