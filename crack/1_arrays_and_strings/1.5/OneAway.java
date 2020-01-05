8557465c811cbd40c6a4498548298882a659b4a9
Igor Veselinovic <igor@veselinovic.net>
2020-01-05 12:11:02 -0500

    Add "One Away" solution

---
+++ crack/1_arrays_and_strings/1.5/OneAway.java
@@ -0,0 +1,38 @@
+public class OneAway {
+    public static boolean oneAway(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        if (Math.abs(len1 - len2) > 1) {
+            return false;
+        } else if (len1 == len2) {
+            boolean foundDiff = false;
+            for (int i = 0; i < len1; i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    if (foundDiff) return false;
+                    foundDiff = true;
+                }
+            }
+            return true;
+        } else {
+            String longStr, shortStr;
+            if (len1 > len2) {
+                longStr = s1;
+                shortStr = s2;
+            } else {
+                longStr = s2;
+                shortStr = s1;
+            }
+            boolean foundDiff = false;
+            for (int i = 0, j = 0; i < Math.max(len1, len2); i++, j++) {
+                if (i == Math.max(len1, len2) - 1 && i == j ||
+                        longStr.charAt(i) != shortStr.charAt(j)) {
+                    if (foundDiff) return false;
+                    foundDiff = true;
+                    j--;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ crack/1_arrays_and_strings/1.5/OneAwayTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class OneAwayTest {
+    @Test
+    public void positive() {
+        assertEquals(true, OneAway.oneAway("pale", "ple"));
+        assertEquals(true, OneAway.oneAway("pales", "pale"));
+        assertEquals(true, OneAway.oneAway("pale", "bale"));
+        assertEquals(true, OneAway.oneAway("ple", "pale"));
+        assertEquals(true, OneAway.oneAway("pale", "pales"));
+    }
+
+    @Test
+    public void negative() {
+        assertEquals(false, OneAway.oneAway("pale", "ples"));
+        assertEquals(false, OneAway.oneAway("pale", "plles"));
+    }
+
+    @Test
+    public void tooLong() {
+        assertEquals(false, OneAway.oneAway("pale", "paleee"));
+    }
+
+    @Test
+    public void emptyString() {
+        assertEquals(true, OneAway.oneAway("", ""));
+        assertEquals(true, OneAway.oneAway("", "1"));
+    }
+
+    @Test
+    public void sameString() {
+        assertEquals(true, OneAway.oneAway("asdf", "asdf"));
+    }
+}
